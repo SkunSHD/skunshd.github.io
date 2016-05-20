8be99a9b1e5ce2750c675327f31df5c0b750ac80
Curious <skunshd@gmail.com>
2016-05-20 20:16:12 +0200

    Save and getAll methods done

--- js/utils/store.js
+++ js/utils/store.js
@@ -2,15 +2,7 @@
 		
 		getAllData: function() {
 			var result = [];
-			for(var i in localStorage) {
-				if(!new Date(i)) continue;
-				var date = new Date(i);
-				
-				var str = localStorage[i];
-				var obj = JSON.parse(str);
-				
-				result.push( {key: date, value: obj} );
+			for(var i in localStorage) {				var obj = {					key: i,					value: localStorage[i]				};				result.push(obj);
 			}
-			
 			return result;
 		},        deleteByKey: function (key) {            localStorage.removeItem(key);        },        deleteAll: function () {            localStorage.clear();        }    };	window.app = window.app || {};	window.app.storage = storage;})(window);
