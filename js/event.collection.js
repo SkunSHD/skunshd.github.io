6e7e7606aee7c8f244c8cbeea3ae0f8608170c17
Curious <skunshd@gmail.com>
2016-05-21 09:21:35 +0200

    End of editing event.model.js

--- js/event.collection.js
+++ js/event.collection.js
@@ -5,49 +5,22 @@
 		list: {},
 		
 		get: function(id) {
-			return this.list.id;
+
 		},
 		
 		checkEvents: function(model) {
-			// hear puted test array with Date key
-			// this.putSomethingInLs(model);
-			
-			// for (var p = 0; p < some.length; p++) {
-				// if(typeof some[p].key.getDate == 'function') alert(some);
-			// }
-			
-			// this.putSomethingInLs(model);]
-			
-			// model.save(new Date() );
-
+			model.getAll();
 		},
 		
 		add: function() {
 			
 		},
-			
-		//This method need to do test array, as like from lS tooked
-		putSomethingInLs: function(model) {
-			var key = new Date().getTime();
-		
-			// var value = '{ "events": [';
-			// for( var i = 0; i < 31; i++) {
-				// value += '{"event": "event test", "date": "' + new Date() + '", "names": "names test", "description": "description test"}';
-				// if(i != 30) value += ',';
-			// }
-			// value += ']}';
-			var array = {events: []};
-			
-			var result = JSON.parse(value);
-			alert(result.events[30].event);
-			// model.save(key, value);
-			// alert(model.getAll().length);
-		},
 		
 		remove: function(id) {
-			// localStorage.removeItem(id);
+
 		}
 	};
+	
 	window.app = window.app || {};
 	window.app.collection = collection;
 })(window);
